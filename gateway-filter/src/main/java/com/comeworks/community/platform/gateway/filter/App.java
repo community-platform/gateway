f1a83ec66aa9ab053d32a63db3c9ae33e5737c16
wangqiang <wqiangwq@cn.ibm.com>
2019-01-09 05:08:03 +0800

    init

---
+++ gateway-filter/src/main/java/com/comeworks/community/platform/gateway/filter/App.java
@@ -0,0 +1,13 @@
+package com.comeworks.community.platform.gateway.filter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
